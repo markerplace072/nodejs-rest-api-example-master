83fc3347bf308b896c1b0383cab63e9e0a091f3a
markerplace072 <marketplace072@outlook.com>
2022-10-07 21:35:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,48 +1,6 @@
-var express = require('express');
-var app = express();
+var http = require('http');
 
-app.set("port", process.env.PORT || 80);
-
-app.get('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.get('/:id', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is GET method with id=" + req.params.id + "." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.post('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is POST method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.put('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is PUT method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-app.delete('/', function (req, res) {
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var response = { "response" : "This is DELETE method." }
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-var server = app.listen(app.get("port"), function () {
-
-  var host = server.address().address
-  var port = server.address().port
-
-  console.log("Node.js API app listening at http://%s:%s", host, port)
-
-})
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(80);
